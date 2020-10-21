40d1cc8d790fbf47ff1976486ae6719cd45f079c
A-vicky <ashleybatchelor79@gmail.com>
2020-10-21 17:23:42 -0600

    trying

--- myblogsite/urls.py
+++ myblogsite/urls.py
@@ -19,6 +19,6 @@ from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls', namespace="blog")),
+    path('', include('blog.urls')),
 
 ]
